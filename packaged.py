ff23c0b965a8256c78f72b9ceaf9d3874dfe8786
wangchenjin <wangchenjin@bytedance.com>
2024-09-29 16:53:01 +0800

    优化mac打包成.app相关

--- packaged.py
+++ packaged.py
@@ -1,12 +1,20 @@
 import os
-import shutil
+import platform
 import sys
 
-env_path = os.path.dirname(sys.executable)
-build_path=f"{os.path.dirname(__file__)}/build"
-
-
 if __name__ == '__main__':
-    command = f'{env_path}/pyinstaller Notepad.spec'.replace('/', os.sep)
-    os.system(command)
-    shutil.rmtree(build_path)
+    env_path = os.path.dirname(sys.executable)
+    pack_path = os.path.dirname(__file__)
+    system_type = platform.system()
+
+    print('编译开始'.center(80, '='))
+
+    if system_type == "Windows":
+        command = f'{env_path}/pyinstaller {pack_path}/OnWindows.spec'.replace('/', os.sep)
+        os.system(command)
+    elif system_type == "Darwin":
+        command = f'{env_path}/pyinstaller {pack_path}/OnMac.spec'.replace('/', os.sep)
+        os.system(command)
+
+    print('编译结束'.center(80, '='))
+    print(f'编译文件存放在{pack_path}/dist/Wally')

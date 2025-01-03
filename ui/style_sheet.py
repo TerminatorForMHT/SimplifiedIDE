14ad7948c78b4ebcdc861e35c70dac34370647b4
wangchenjin <wangchenjin@bytedance.com>
2025-01-03 17:06:52 +0800

    优化mac打包成.app相关

--- ui/style_sheet.py
+++
@@ -1,9 +0,0 @@
-ButtonStyleSheet = """
-    QPushButton{
-        background-color: rgba(255, 255, 255, 0);
-        border: 0px;
-    }   
-    QPushButton:hover {
-        background-color: rgb(72, 140, 245);
-    }
-"""
